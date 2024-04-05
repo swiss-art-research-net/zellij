3aaa8aa6fb3769194121048eb87c978e8012c555
Mike Griniezakis <griniezakismike12@gmail.com>
2024-04-05 12:34:40 +0300

    export more fields from models

--- website/exporters/ModelExporter.py
+++ website/exporters/ModelExporter.py
@@ -19,7 +19,12 @@ class ModelExporter(Exporter):
 
     def _generate_xml(self) -> str:
         root = ET.Element("composite_semantic_pattern")
-        names = ET.SubElement(root, "names")
+        definition = ET.SubElement(root, "definition")
+
+        type_el = ET.SubElement(definition, "type")
+        type_el.text = "Model"
+
+        names = ET.SubElement(definition, "names")
         for result in self._results:
             if result.get("KeyField") != self._item:
                 continue
@@ -39,11 +44,11 @@ class ModelExporter(Exporter):
                     name_language.text = "English"
 
                 if self._prefill_group.get(key, {}).get('name') == "Identifier":
-                    id = ET.SubElement(root, "system_identifier")
-                    id.text = val
+                    system_identifier = ET.SubElement(definition, "system_identifier")
+                    system_identifier.text = val
 
                 if self._prefill_group.get(key, {}).get('name') == "Description":
-                    descriptions = ET.SubElement(root, "descriptions")
+                    descriptions = ET.SubElement(definition, "descriptions")
                     description = ET.SubElement(descriptions, "description")
                     description_content = ET.SubElement(description, "description_content")
                     description_content.text = val
@@ -58,7 +63,7 @@ class ModelExporter(Exporter):
                     description_preference.text = "Preferred"
 
                 if self._prefill_group.get(key, {}).get('name') == "Ontological_Scope":
-                    ontological_scopes = ET.SubElement(root, "ontological_scopes")
+                    ontological_scopes = ET.SubElement(definition, "ontological_scopes")
                     for record_id in val:
                         record = self._airtable.get_record_by_id('CRM Class', record_id)
 
@@ -69,8 +74,41 @@ class ModelExporter(Exporter):
                         class_uri = ET.SubElement(ontology_class, "class_URI")
                         class_uri.text = record.get("fields", {}).get("Subject")
 
+                if self._prefill_group.get(key, {}).get('name') == "Model_NameSpaces":
+                    semantic_context = ET.SubElement(root, "semantic_context")
 
+                if self._prefill_group.get(key, {}).get('name') == "Fields_Expected_Resource_Model":
+                    pattern_context = ET.SubElement(root, "pattern_context")
 
+                    semantic_patterns_deployed_in = ET.SubElement(pattern_context, "semantic_patterns_deployed_in")
+                    target_of = ET.SubElement(semantic_patterns_deployed_in, "target_of")
+
+                    for record_id in val:
+                        field = self._airtable.get_record_by_id('Field', record_id)
+
+                        atomic_semantic_pattern = ET.SubElement(target_of, "atomic_semantic_pattern")
+                        atomic_semantic_pattern_name = ET.SubElement(atomic_semantic_pattern, "atomic_semantic_pattern_name")
+                        atomic_semantic_pattern_name.text = field.get("fields", {}).get("UI_Name")
+
+                        atomic_semantic_pattern_uri = ET.SubElement(atomic_semantic_pattern, "atomic_semantic_pattern_URI")
+                        atomic_semantic_pattern_uri.text = field.get("fields", {}).get("URI").strip()
+
+                        composite_semantic_pattern_type = ET.SubElement(atomic_semantic_pattern, "composite_semantic_pattern_type")
+                        composite_semantic_pattern_type.text = "Model"
+
+                if self._prefill_group.get(key, {}).get('name') == "Model_Fields":
+                    composition = ET.SubElement(root, "composition")
+
+                    for record_id in val:
+                        model_field = self._airtable.get_record_by_id('Model_Fields', record_id)
+                        pattern = ET.SubElement(composition, "pattern")
+
+                        pattern_name = ET.SubElement(pattern, "pattern_name")
+                        field_ui_names = model_field.get("fields", {}).get("Field_UI_Name")
+                        pattern_name.text = field_ui_names[0] if len(field_ui_names) > 0 else ''
+
+                        pattern_uri = ET.SubElement(pattern, "pattern_URI")
+                        pattern_uri.text = "uri"
 
         rough_string = ET.tostring(root, "utf-8")
         reparsed = minidom.parseString(rough_string)
