aefae5d3995b63c0de39d31d1f802317dfe7f04f
Mike Griniezakis <griniezakismike12@gmail.com>
2024-06-27 13:39:13 +0300

    update model exporter

--- website/exporters/ModelExporter.py
+++ website/exporters/ModelExporter.py
@@ -13,6 +13,8 @@ class ModelExporter(Exporter):
     def _generate_xml(self) -> str:
         root = ET.Element("composite_semantic_pattern")
         definition = ET.SubElement(root, "definition")
+        semantic_context = ET.SubElement(root, "semantic_context")
+        pattern_context = ET.SubElement(root, "pattern_context")
 
         type_el = ET.SubElement(definition, "type")
         type_el.text = self._selected_scheme
@@ -27,13 +29,13 @@ class ModelExporter(Exporter):
             if result.get("KeyField") != self._item:
                 continue
             for key, val in result.items():
-                if not self._prefill_group.get(key, {}).get("exportable", False):
+                if not self._prefill_group.get(key, {}).get("exportable", False) and key != "KeyField":
                     continue
 
                 if self._prefill_group.get(key, {}).get('name') == "UI_Name":
                     name = ET.SubElement(names, "name")
-                    nameContent = ET.SubElement(name, "name_content")
-                    nameContent.text = val
+                    name_content = ET.SubElement(name, "name_content")
+                    name_content.text = val
 
                     name_type = ET.SubElement(name, "name_type")
                     name_type.text = "UI_Name"
@@ -41,9 +43,41 @@ class ModelExporter(Exporter):
                     name_language = ET.SubElement(name, "name_language")
                     name_language.text = "English"
 
+                if self._prefill_group.get(key, {}).get('name') == "System_Name":
+                    system_name = ET.SubElement(definition, "system_name")
+                    system_name.text = val
+
                 if self._prefill_group.get(key, {}).get('name') == "Identifier":
                     system_identifier = ET.SubElement(definition, "system_identifier")
                     system_identifier.text = val
+
+                if self._prefill_group.get(key, {}).get('name') == "Ontology_Context":
+                    ontologies = ET.SubElement(semantic_context, "ontologies")
+                    for record_id in val:
+                        record = self._airtable.get_record_by_id('Ontology', record_id)
+                        ontology = ET.SubElement(ontologies, "ontology")
+
+                        ontology_prefix = ET.SubElement(ontology, "ontology_prefix")
+                        ontology_prefix.text = record.get("fields", {}).get("Prefix")
+
+                        ontology_uri = ET.SubElement(ontology, "ontology_URI")
+                        ontology_uri.text = record.get("fields", {}).get("Namespace")
+
+                        ontology_name = ET.SubElement(ontology, "ontology_name")
+                        ontology_name.text = record.get("fields", {}).get("UI_Name")
+
+                        ontology_version = ET.SubElement(ontology, "ontology_version")
+                        ontology_version.text = record.get("fields", {}).get("Version")
+
+                if key == "KeyField":
+                    identifiers = ET.SubElement(definition, "identifiers")
+                    identifier = ET.SubElement(identifiers, "identifier")
+
+                    identifier_content = ET.SubElement(identifier, "identifier_content")
+                    identifier_content.text = val
+
+                    identifier_type = ET.SubElement(identifier, "identifier_type")
+                    identifier_type.text = "unique identifier"
 
                 if self._prefill_group.get(key, {}).get('name') == "Description":
                     descriptions = ET.SubElement(definition, "descriptions")
@@ -75,9 +109,21 @@ class ModelExporter(Exporter):
                 if self._prefill_group.get(key, {}).get('name') == "Model_NameSpaces":
                     semantic_context = ET.SubElement(root, "semantic_context")
 
-                if self._prefill_group.get(key, {}).get('name') == "Fields_Expected_Resource_Model" or self._prefill_group.get(key, {}).get('name') == "Fields_Expected_Collection_Model":
-                    pattern_context = ET.SubElement(root, "pattern_context")
+                if self._prefill_group.get(key, {}).get('name') == "Project":
+                    for record_id in val:
+                        record = self._airtable.get_record_by_id('Project', record_id)
 
+                        semantic_pattern_space = ET.SubElement(pattern_context, "semantic_pattern_space")
+
+                        semantic_pattern_space_name = ET.SubElement(semantic_pattern_space, "semantic_pattern_space_name")
+                        semantic_pattern_space_name.text = record.get("fields", {}).get("UI_Name")
+
+                        semantic_pattern_space_uri = ET.SubElement(semantic_pattern_space, "semantic_pattern_space_URI")
+                        semantic_pattern_space_uri.text = record.get("fields", {}).get("Namespace")
+
+                if (self._prefill_group.get(key, {}).get('name') == "Fields_Expected_Resource_Model" or
+                    self._prefill_group.get(key, {}).get('name') == "Fields_Expected_Collection_Model"
+                ):
                     semantic_patterns_deployed_in = ET.SubElement(pattern_context, "semantic_patterns_deployed_in")
                     target_of = ET.SubElement(semantic_patterns_deployed_in, "target_of")
 
@@ -98,15 +144,23 @@ class ModelExporter(Exporter):
                     referenced_table = self._prefill_group.get(key, {}).get('name')
                     composition = ET.SubElement(root, "composition")
 
+                    fields_to_populate = {}
+                    fields_uris = []
+
                     for model_field in self._airtable.get_multiple_records_by_formula(referenced_table, OR(*list(map(lambda x: EQUAL(STR_VALUE(x), 'RECORD_ID()'), val)))):
                         pattern = ET.SubElement(composition, "pattern")
 
-                        pattern_name = ET.SubElement(pattern, "pattern_name")
+                        atomic_semantic_pattern_name = ET.SubElement(pattern, "atomic_semantic_pattern_name")
                         field_ui_names = model_field.get("fields", {}).get("Field_UI_Name")
-                        pattern_name.text = field_ui_names[0] if len(field_ui_names) > 0 else ''
+                        atomic_semantic_pattern_name.text = field_ui_names[0] if len(field_ui_names) > 0 else ''
 
-                        pattern_uri = ET.SubElement(pattern, "pattern_URI")
-                        pattern_uri.text = "uri"
+                        atomic_semantic_pattern_uri = ET.SubElement(pattern, "atomic_semantic_pattern_URI")
+                        if len(model_field.get("fields", {}).get("Field")) > 0:
+                            fields_to_populate[model_field.get("fields", {}).get("Field")[0]] = atomic_semantic_pattern_uri
+                            fields_uris.append(model_field.get("fields", {}).get("Field")[0])
+
+                    for field in self._airtable.get_multiple_records_by_formula('Field', OR(*list(map(lambda x: EQUAL(STR_VALUE(x), 'RECORD_ID()'), fields_uris)))):
+                        fields_to_populate[field['id']].text = field.get("fields", {}).get("URI")
 
                 if self._prefill_group.get(key, {}).get('name') == "Total_SparQL":
                     serialization = ET.SubElement(root, "serialization")
@@ -151,15 +205,22 @@ class ModelExporter(Exporter):
                     for record_id in val:
                         author = self._airtable.get_record_by_id('Actors', record_id)
                         creator = ET.SubElement(creators, "creator")
+                        creator.attrib['uri'] = author.get("fields", {}).get("URI", '')
 
                         creator_name = ET.SubElement(creator, "creator_name")
                         creator_name.text = author.get("fields", {}).get("Name")
 
-                        creator_URI = ET.SubElement(creator, "creator_URI")
-                        creator_URI.text = author.get("fields", {}).get("URI", '')
+                        creator_uri = ET.SubElement(creator, "creator_URI")
+                        creator_uri.text = author.get("fields", {}).get("URI", '')
 
                 if self._prefill_group.get(key, {}).get('name') == "Funders":
                     funder = ET.SubElement(funding, "funder")
+
+                    for record_id in val:
+                        funder_record = self._airtable.get_record_by_id('Institution', record_id)
+
+                        funder_name = ET.SubElement(funder, "funder_name")
+                        funder_name.text = funder_record.get("fields", {}).get("Name")
 
         rough_string = ET.tostring(root, "utf-8")
         reparsed = minidom.parseString(rough_string)
