4d975273eaeea09f978380ab46c2cf130b7bdfd1
Mike Griniezakis <griniezakismike12@gmail.com>
2024-07-23 16:05:11 +0300

    add support for uploading files

--- website/docs.py
+++ website/docs.py
@@ -10,8 +10,9 @@ from io import BytesIO
 from flask import Blueprint, render_template, request, abort, Response
 
 from ZellijData.AirTableConnection import AirTableConnection, EnhancedResponse
+from github_wrapper import GithubWrapper
 from website.datasources import get_prefill
-from website.db import get_db, dict_gen_many, generate_airtable_schema, decrypt
+from website.db import get_db, dict_gen_many, generate_airtable_schema, decrypt, dict_gen_one
 from website.exporters.ModelExporter import ModelExporter
 from website.exporters.FieldExporter import FieldExporter
 from website.exporters.ProjectExporter import ProjectExporter
@@ -259,6 +260,15 @@ def patternlistexporttree(apikey, exportType, model):
         'project': ProjectExporter,
     }
 
+    database = get_db()
+    c = database.cursor()
+    c.execute("SELECT * FROM AirTableDatabases WHERE dbaseapikey=%s", (apikey,))
+    existing = dict_gen_one(c)
+    if existing["githubtoken"]:
+        existing["githubtoken"] = decrypt(existing["githubtoken"])
+
+    github = GithubWrapper(existing["githubtoken"], existing["githubrepo"])
+
     item = request.args.get("item")
 
     files = []
@@ -266,6 +276,7 @@ def patternlistexporttree(apikey, exportType, model):
         exporter = exporters[exportType]().initialize(model, apikey, item)
         file = exporter.export()
         files.append({"name": exporter.get_name(), "file": file})
+        github.upload_file(f"space/{exporter.get_name()}.xml", file)
 
         schemas, secretkey = generate_airtable_schema(apikey)
         airtable = AirTableConnection(decrypt(secretkey), apikey)
---
+++ website/github_wrapper.py
@@ -0,0 +1,26 @@
+import io
+from github import Github, Auth
+from github.Repository import Repository
+
+
+class GithubWrapper:
+    github: Github
+    repo: Repository
+
+    def __init__(self, token: str, repo: str):
+        self.github = Github(auth=Auth.Token(token))
+        self.repo = self.github.get_user().get_repo(repo)
+
+    def upload_file(self, file_path: str, content: io.BytesIO):
+        content_file = None
+        try:
+            content_file = self.repo.get_contents(file_path)
+            if isinstance(content_file, list):
+                content_file = content_file[0]
+        except Exception as e:
+            pass
+
+        if content_file is not None:
+            self.repo.update_file(file_path, "Update file", content.getvalue(), content_file.sha)
+        else:
+            self.repo.create_file(file_path, "Create file", content.getvalue())
