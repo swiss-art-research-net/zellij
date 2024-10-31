29230fe1e25323204cfdc945860bf0a855985ddc
Mike Griniezakis <34035338+MikeGriniezakis@users.noreply.github.com>
2024-10-31 12:42:36 +0200

    Feature/UI implementations (#102)
    * move the attributes table to the top of the page #92
    
    * fix select buttons on macos
    
    * prevent attributes table fields from overflowing #88
    
    * add about page
    
    * fix the derivatives header
    
    * add about page template
    
    * show the labels instead of the record id

--- website/pages.py
+++ website/pages.py
@@ -6,6 +6,8 @@ Created on Mar. 11, 2021
 
 from flask import Blueprint, render_template
 
+from website.db import dict_gen_many, get_db
+
 
 bp = Blueprint("pages", __name__)
 
@@ -13,3 +15,28 @@ bp = Blueprint("pages", __name__)
 @bp.route("/", methods=["GET"])
 def mainpage():
     return render_template("big.html")
+
+@bp.route("/about", methods=["GET"])
+def about():
+    db = get_db()
+    c = db.cursor()
+
+    c.execute(
+        "SELECT *, COUNT(dbasekey) AS Count FROM AirTableDatabases"
+        + " LEFT JOIN AirTableAccounts"
+        + " ON accountid = airtableaccountkey"
+        + " LEFT JOIN Scrapers"
+        + " ON dbaseid = dbasekey"
+        + " WHERE scraperid IS NOT NULL"
+        + " GROUP BY dbasekey"
+    )
+    dblist = [d for d in dict_gen_many(c)]
+
+    grouped_dbs = {}
+
+    for db in dblist:
+        if db["accountname"] not in grouped_dbs:
+            grouped_dbs[db["accountname"]] = []
+        grouped_dbs[db["accountname"]].append(db)
+
+    return render_template("about-page.html", projects=list(grouped_dbs.keys()))
